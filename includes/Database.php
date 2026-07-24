194c3eadab30dafa20725708a3a5576dfb00f8bc
AGYEISTEPHEN <agyeistephen107@gmail.com>
2026-07-24 04:26:40 +0000

    Improve code

--- includes/Database.php
+++ includes/Database.php
@@ -14,7 +14,7 @@ class Database {
         if (self::$instance === null) {
             $dsn = sprintf(
                 'mysql:host=%s;port=%s;dbname=%s;charset=%s',
-                DB_HOST, DB_PORT, DB_NAME, DB_CHARSET
+                DB_HOST, DB_NAME, DB_CHARSET
             );
             try {
                 self::$instance = new PDO($dsn, DB_USER, DB_PASS, [
